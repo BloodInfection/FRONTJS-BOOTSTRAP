06b982bf7ca014330077feba41cb35f16ba491f4
Anastasia Filatova <56803387+BloodInfection@users.noreply.github.com>
2022-03-31 19:02:59 +0300

    product-api methodts

--- src/services/product-api.service.js
+++ src/services/product-api.service.js
@@ -4,30 +4,108 @@ axios.defaults.withCredentials = true
 
 //TODO создать некоторые ручки для продукт апи
 class ProductApiService {
-  login(email, password) {
+
+  brand (id) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "brand", {params:{id}})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+  echo (message) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "echo", {params:{message}})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+  category (number, limit, name, level) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "category", {params:{"page.number": number, "page.limit": limit, name, level }})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+  color(id) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "color", {params:{id}})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+  size(id) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "size", {params:{id}})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+  final_roduct(id) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "final-product", {params:{id}})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+  full_product (productId) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "full-product", {params:{productId}})
+    .then((response)=>{
+      return response.data;
+    }); 
+    }
+
+
+  final_roductList(productId) {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "final-product/list", {params:{productId}})
+    .then((response)=>{
+      return response.data;
+    }); 
+  }
+
+
+  sizeList(){
     return axios
-      .post(PRODUCTRAPI_URL + "login", { email, password })
-      .then((response) => {
-        if (response.data.sessionID) {
-          localStorage.setItem("sessionID", JSON.stringify(response.data));
-        }
+    .get(PRODUCTRAPI_URL + "size/list")
+    .then((response)=>{
         return response.data;
-      });
+      }
+    );
   }
-  logout() {
-	return axios
-	.post(PRODUCTRAPI_URL + "logout", {})
-	.then((response) => {
-		localStorage.removeItem("sessionID");
-		return response.data;
-	});
-  }
-  register(name, password, surname, patronymic, phone, email) {
-    return axios
-	.post(PRODUCTRAPI_URL + "user", { name, password, surname, patronymic, phone, email })
-	.then((response)=>{
+
+  listColor () {
+    return axios //класс с методами:
+	.get(PRODUCTRAPI_URL + "color/list")
+  .then((response)=>{
 		return response.data;
 	}); 
   }
+
+  listProducts  (number, limit, name, categoryId, brandId) {
+    return axios //класс с методами:
+	.get(PRODUCTRAPI_URL + "product/list", {params:{"page.number": number, "page.limit": limit, name, categoryId, brandId }})
+  .then((response)=>{
+		return response.data;
+	}); 
+  }
+
+
+  listBrand () {
+    return axios //класс с методами:
+    .get(PRODUCTRAPI_URL + "brand/list")
+    .then((response)=>{
+      return response.data;
+    }); 
+    }
+
+  
+
 }
 export default new ProductApiService();
